e2ac37572d8c30714a7af29851b72ebbc80cd497
adcimon <37084114+adcimon@users.noreply.github.com>
2021-08-11 17:21:02 +0200

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap()
     });
 
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.enableCors();
 
     await app.listen(process.env.PORT || 3000);
 }
