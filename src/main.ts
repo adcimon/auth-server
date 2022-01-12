2350a8d18171a04e52fa489e01479371476ce87f
adcimon <37084114+adcimon@users.noreply.github.com>
2022-01-12 16:40:58 +0100

    Added HTTPS support

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
 import { ConfigService } from './config/config.service';
+import * as fs from 'fs';
 
 async function bootstrap()
 {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    let httpsOptions = { };
+    if( ConfigService.get('ENABLE_HTTPS') )
+    {
+        httpsOptions['key'] = fs.readFileSync(ConfigService.get('KEY_PATH'));
+        httpsOptions['cert'] = fs.readFileSync(ConfigService.get('CERT_PATH'))
+    }
+
+    const app = await NestFactory.create<NestExpressApplication>(AppModule,
+    {
+        httpsOptions: httpsOptions
+    });
     app.useGlobalFilters(new HttpExceptionFilter());
     if( ConfigService.get('ENABLE_CORS') )
     {
